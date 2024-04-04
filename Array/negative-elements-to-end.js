f90e4bef08035fbc822791f03ccd523fbcaddfea
d.aikema <djurreaikema@gmail.com>
2024-04-04 13:54:45 +0200

    Array - 0s 1s and 2s

---
+++ Array/negative-elements-to-end.js
@@ -0,0 +1,11 @@
+// https://www.geeksforgeeks.org/problems/move-all-negative-elements-to-end1813/1
+// Move all negative elements to end
+
+class Solution {
+  segregateElements(arr, n) {
+
+  }
+}
+
+const solution = new Solution();
+console.log(solution.segregateElements([1, -1, 3, 2, -7, -5, 11, 6], 8));
--- Array/sort-an-array-of-0s-1s-and-2s.js
+++ Array/sort-an-array-of-0s-1s-and-2s.js
@@ -1,17 +1,28 @@
 // https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
 // Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
 
-// TODO
+// Answer given - challenge failed
 class Solution {
+  sort012(a, n) {
+    let low = 0, high = n - 1, mid = 0; // Initialize three pointers: low, mid, and high.
+    while (mid <= high) { // Iterate until mid is greater than high.
+      if (a[mid] === 0) { // If the element at mid is 0.
+        let temp = a[low]; // Swap the element at low with the element at mid.
+        a[low] = a[mid];
+        a[mid] = temp;
+        low++; // Increment low.
+        mid++; // Increment mid.
+      } else if (a[mid] === 1) { // If the element at mid is 1.
+        mid++; // Increment mid.
+      } else { // If the element at mid is 2.
+        let temp = a[mid]; // Swap the element at mid with the element at high.
+        a[mid] = a[high];
+        a[high] = temp;
+        high--; // Decrement high.
+      }
+    }
 
-  sort012(arr, N) {
-    [...arr].forEach((number, index) => {
-      if (number !== 1) arr.splice(index, 1);
-      if (number === 0) arr.unshift(0);
-      if (number === 2) arr.push(2);
-    });
-
-    return arr;
+    return a;
   }
 }
 
