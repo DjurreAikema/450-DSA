f884df8b1ec4332315c5673751cad1cbe49c0615
d.aikema <djurreaikema@gmail.com>
2024-04-05 09:49:06 +0200

    Array - Move all negative elements to end

--- Array/negative-elements-to-end.js
+++ Array/negative-elements-to-end.js
@@ -3,9 +3,23 @@
 
 class Solution {
   segregateElements(arr, n) {
+    let index = 0;
 
+    for (let j = 0; j < n; j++) {
+      let currVal = arr[index];
+      if (currVal < 0) {
+        arr.splice(index, 1);
+        arr.push(currVal);
+      } else {
+        index++;
+      }
+    }
+
+    return arr;
   }
 }
 
 const solution = new Solution();
+console.time('segregateElements');
 console.log(solution.segregateElements([1, -1, 3, 2, -7, -5, 11, 6], 8));
+console.timeEnd('segregateElements');
