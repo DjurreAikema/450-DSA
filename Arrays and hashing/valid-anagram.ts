3825be0fc315bead695ee0b0cb8033d9386024bb
d.aikema <djurreaikema@gmail.com>
2024-04-11 16:38:25 +0200

    Array - Valid Anagram

--- Arrays and hashing/valid-anagram.ts
+++ Arrays and hashing/valid-anagram.ts
@@ -6,16 +6,20 @@
 function isAnagram(s: string, t: string): boolean {
   if (s.length != t.length) return false;
 
+  // Create two maps to keep track of the number of times a letter appears in the string
   const sCount = new Map<string, number>();
   const tCount = new Map<string, number>();
 
+  // Loop over both strings, this can be done in one loop because we're sure the strings are the same length
   for (let i: number = 0; i < s.length; i++) {
     const sChar: string = s.charAt(i);
     sCount.set(sChar, (sCount.get(sChar) || 0) + 1);
+
     const tChar: string = t.charAt(i);
     tCount.set(tChar, (tCount.get(tChar) || 0) + 1);
   }
 
+  // Compare the maps
   for (const [char, count] of sCount) {
     if (tCount.get(char) !== count) return false;
   }
