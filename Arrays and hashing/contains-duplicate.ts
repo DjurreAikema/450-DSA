6053de83fb18f032ce8abc2337451240a8118488
d.aikema <djurreaikema@gmail.com>
2024-04-11 15:59:35 +0200

    Array - Contains Duplicate

---
+++ Arrays and hashing/contains-duplicate.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/contains-duplicate/
+// 217. Contains Duplicate
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+function containsDuplicate(nums: number[]): boolean {
+  const mySet: Set<number> = new Set();
+  let lastSize: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    mySet.add(nums[i]);
+    if (mySet.size == lastSize) return true;
+    lastSize = mySet.size;
+  }
+
+  return false;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
