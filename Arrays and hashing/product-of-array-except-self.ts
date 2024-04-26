ca71f7bc2b800219573073399d7042db3552942c
d.aikema <djurreaikema@gmail.com>
2024-04-26 09:35:13 +0200

    Array - 238. Product of Array Except Self

--- Arrays and hashing/product-of-array-except-self.ts
+++ Arrays and hashing/product-of-array-except-self.ts
@@ -2,8 +2,27 @@
 // 238. Product of Array Except Self
 // Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
-// Passed, 113ms, 64.22mb
+// Passed, 104ms, 66.25mb
 function productExceptSelf(nums: number[]): number[] {
+  const n: number = nums.length;
+  const resultArr: number[] = new Array(n)
+
+  resultArr[0] = 1;
+  for (let i = 1; i < n; i++) {
+    resultArr[i] = resultArr[i - 1] * nums[i - 1];
+  }
+
+  let rightProduct: number = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    resultArr[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return resultArr;
+}
+
+// Passed, 113ms, 64.22mb
+function productExceptSelf2(nums: number[]): number[] {
 
   let resultArr: number[] = [];
 
